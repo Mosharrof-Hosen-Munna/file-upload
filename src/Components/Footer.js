c0dfece891a80a7c836e07c0d253ae0949449019
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2024-04-18 15:49:57 +0600

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-center py-20 bg-gray-100'>
+        <div>
+        © 2024 Copyright: fileupload
+        </div>
+    </div>
+  )
+}
+
+export default Footer
